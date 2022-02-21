dbd89d93c03ce9c871783fedf6804be5761349f1
Anzo <antonr@live.dk>
2022-02-21 22:28:24 +0100

    Pawns can no longer phase through pieces on their first move

--- src/main/java/dk/anzodk/skakproject/Pawn.java
+++ src/main/java/dk/anzodk/skakproject/Pawn.java
@@ -44,24 +44,6 @@ public class Pawn extends ChessPiece{
         if(showMoves)
         {
             int fieldMod = (isWhite ? 1 : -1);
-            if(!hasMoved)
-            {
-                if(!ChessController.INSTANCE.ContainsKey(new ChessPos(currPos.row,currPos.column+(fieldMod*2))))
-                {
-                    ImageView t = new ImageView();
-                    t.setImage(ChessController.INSTANCE.__Move_Image__);
-                    t.setFitHeight(GameController.SpaceHeight);
-                    t.setFitWidth(GameController.SpaceWidth);
-                    t.setLayoutX((44+1) + (((int)currPos.row - ((int)'a')) * GameController.SpaceWidth));
-                    t.setLayoutY((38+1) + ((currPos.column+(fieldMod*2)-1) * GameController.SpaceHeight));
-                    String id = "";
-                    id += currPos.row;
-                    id += currPos.column+(fieldMod*2);
-                    t.setId(id);
-                    GameController.GamePane.getChildren().add(t);
-                    currMoves.add(t);
-                }
-            }
             if(!ChessController.INSTANCE.ContainsKey(new ChessPos(currPos.row,currPos.column+fieldMod)))
             {
                     ImageView t = new ImageView();
@@ -76,6 +58,23 @@ public class Pawn extends ChessPiece{
                     t.setId(id);
                     GameController.GamePane.getChildren().add(t);
                     currMoves.add(t);
+                    if (!hasMoved) 
+                    {
+                        if (!ChessController.INSTANCE.ContainsKey(new ChessPos(currPos.row, currPos.column + (fieldMod * 2)))) {
+                            ImageView t2 = new ImageView();
+                            t2.setImage(ChessController.INSTANCE.__Move_Image__);
+                            t2.setFitHeight(GameController.SpaceHeight);
+                            t2.setFitWidth(GameController.SpaceWidth);
+                            t2.setLayoutX((44 + 1) + (((int) currPos.row - ((int) 'a')) * GameController.SpaceWidth));
+                            t2.setLayoutY((38 + 1) + ((currPos.column + (fieldMod * 2) - 1) * GameController.SpaceHeight));
+                            String id2 = "";
+                            id2 += currPos.row;
+                            id2 += currPos.column + (fieldMod * 2);
+                            t2.setId(id2);
+                            GameController.GamePane.getChildren().add(t2);
+                            currMoves.add(t2);
+                        }
+                    }
             }
             if(ChessController.INSTANCE.ContainsKey(new ChessPos((char)((int)currPos.row+1),currPos.column+fieldMod)) && currPos.row != 'h')
             {

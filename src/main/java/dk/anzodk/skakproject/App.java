1158236da6d4b91410f1dbf52f2557ee80f5a72b
Anzo <antonr@live.dk>
2022-02-21 17:56:51 +0100

    sizefix

--- src/main/java/dk/anzodk/skakproject/App.java
+++ src/main/java/dk/anzodk/skakproject/App.java
@@ -19,7 +19,8 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 823, 556);
+        //scene = new Scene(loadFXML("primary"), 823, 556);
+        scene = new Scene(loadFXML("primary"));
         App._stage = stage;
         stage.setScene(scene);
         stage.addEventFilter(WindowEvent.WINDOW_CLOSE_REQUEST, eh -> {
